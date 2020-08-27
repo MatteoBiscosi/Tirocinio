3bf9d37a5b88f189dfb278760ed82d0154734bf4
MatteoBiscosi <m.biscosi.97@gmail.com>
2020-08-27 10:57:17 +0200

    What's happening here

--- nDPILight/src/lib/NapatechReader.cpp
+++ nDPILight/src/lib/NapatechReader.cpp
@@ -242,30 +242,31 @@ void NapatechReader::taskReceiverAny(const char* streamName, NtFlowStream_t& flo
     int status;
     uint64_t idCounter = 0U;
     std::vector<std::unique_ptr<NtFlow_t>> learnedFlowList;   
-    
-    
+int i = 0;
+
     NT_NetRxOpen(&(this->hNetRxMiss), "Miss packets stream", NT_NET_INTERFACE_PACKET, STREAM_ID_MISS, -1);
     if(handleErrorStatus(status, "NT_NetRxOpen() failed") != 0)
-        return;
+	    return;
 
 
     while(this->error_or_eof == 0) {
-        // Get package from rx stream.
-        status = NT_NetRxGet(this->hNetRxMiss, &(this->hNetBufferMiss), -1);
-	        
-        if(status == NT_STATUS_TIMEOUT || status == NT_STATUS_TRYAGAIN) 
-            continue;
+	    // Get package from rx stream.
+	    status = NT_NetRxGet(this->hNetRxMiss, &(this->hNetBufferMiss), -1);
+printf("Counter %d\n", ++i);
+	    if(status == NT_STATUS_TIMEOUT || status == NT_STATUS_TRYAGAIN) 
+		    continue;
 
-        if(status == NT_ERROR_NT_TERMINATING)
-	    break;
+	    if(status == NT_ERROR_NT_TERMINATING)
+		    break;
 
-        if(handleErrorStatus(status, "Error while sniffing next packet") != 0)
-            continue;
-	
-        pkt_parser->processPacket(this, &(this->hNetBufferMiss), nullptr);
-	
-        if(this->newFlowCheck == true) {
-            //std::cout << "New flow\n";
+	    if(handleErrorStatus(status, "Error while sniffing next packet") != 0)
+		    continue;
+
+/*	    pkt_parser->processPacket(this, &(this->hNetBufferMiss), nullptr);
+/*
+	    if(this->newFlowCheck == true) {
+		printf("flow check %b\n", this->newFlowCheck);            
+//std::cout << "New flow\n";
             // Here a package has successfully been received, and the parameters for the
             // next flow to be learned will be set up.
             auto flow = std::unique_ptr<NtFlow_t>(new NtFlow_t);
@@ -332,7 +333,7 @@ void NapatechReader::taskReceiverAny(const char* streamName, NtFlowStream_t& flo
             learnedFlowList.push_back(std::move(flow));
 	    this->setNewFlow(false);
         }
-	
+*/	
         status = NT_NetRxRelease(this->hNetRxMiss, this->hNetBufferMiss);
         if(handleErrorStatus(status, "Error while releasing packet") != 0)
             continue;
@@ -408,10 +409,10 @@ int NapatechReader::startRead()
     /* Analysis starts */
     tracer->traceEvent(2, "\tAnalysis started\r\n\r\n");
 
-    std::thread receiverThread2(taskReceiverUnh, "flowmatch_example_receiver_net_rx_unhandled", this);
+    //std::thread receiverThread2(taskReceiverUnh, "flowmatch_example_receiver_net_rx_unhandled", this);
     this->taskReceiverAny("flowmatch_example_receiver_net_rx_miss", flowStream);
 
-    receiverThread2.join();
+   // receiverThread2.join();
 
     return 0;
 }
